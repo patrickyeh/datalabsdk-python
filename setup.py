89629fc06db299c761a838c08fa0df521db3b142
PatrickYeh <patrickyeh@iii.org.tw>
2015-06-24 15:04:10 +0800

    Assign python version to setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,19 @@ __author__ = 'Vetom'
 from setuptools import setup,find_packages
 
 setup(name='datalabsdk',
-      version='0.1',
+      version='0.1.1',
       description='Data Lab API for Python',
       author='III Data Analytic Team',
+      author_email='patrickyeh@iii.org.tw',
       install_requires=['numpy'],
-      packages=['datalabsdk'])
+      url='https://github.com/patrickyeh/datalabsdk-python',
+      packages=['datalabsdk'],
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7']
+      )
